d2b0b209cef17b4dc9f6454a088146ad829d6004
Zahra Kalsoom <zahra@example.com>
2025-08-01 00:22:09 +0500

    Describe what you changed here

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,12 +9,13 @@ export default function hero() {
             
        <h1 className=" text-5xl font-semibold  w-full  max-w-2xl text-center text-cyan-900 ">The Future of Manufacturing with latest Technology</h1>
        <p className=" text-90 mt-4 w-full  max-w-lg text-center ">
-           Expert tech elevate your manufacturing let's take your business further.
+           Expert tech elevate your manufacturing let&apos;s take your business further.
         </p>
        
        <div className="flex items-center justify-center space-x-4 p-6">
-  <button className="px-6 py-2 bg-cyan-900 text-white rounded-full">Get Started</button>
-  <button className="px-6 py-2 bg-white text-cyan-900 rounded-full">Try demo</button>
+        <Button className="px-6 py-2 bg-cyan-900 text-white rounded-full">Get Started</Button>
+       <Button className="px-6 py-2 bg-white text-cyan-900 rounded-full hover:text-white">Try demo</Button>
+  
 </div>
       </div>
       </div>
